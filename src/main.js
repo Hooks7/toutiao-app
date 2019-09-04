906c4f01b0c6aacf5eaa9e04ac9bb1e834b02255
Hooks7 <youremail@example.com>
2019-09-04 10:22:38 +0800

    表单验证中文包

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant' // 导入vant
-import VeeValidate from 'vee-validate' // 导入表单验证
+import VeeValidate, { Validator } from 'vee-validate' // 导入表单验证
+// 导验证中文包
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import 'vant/lib/index.css'
 import './styles/index.css' // 导入通用样式
 
 Vue.use(Vant)
 // 配置
 Vue.use(VeeValidate)
+Validator.localize('zh_CN', zhCN)
 Vue.config.productionTip = false
 
 new Vue({
