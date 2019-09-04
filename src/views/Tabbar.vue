3361fc4a8738723d43992028d1e61381a7703b23
Hooks7 <youremail@example.com>
2019-09-04 14:34:10 +0800

    标签栏

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,16 +1,24 @@
 <template>
- <div>
-     <router-view></router-view>
-     只是
- </div>
+  <div>
+    <router-view></router-view>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">主页</van-tabbar-item>
+      <van-tabbar-item icon="records">问答</van-tabbar-item>
+      <van-tabbar-item icon="tv-o">视频</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
