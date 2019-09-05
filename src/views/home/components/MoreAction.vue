ca4b12db1e40c7f34c9f3fc0d915cfea09732109
Hooks7 <youremail@example.com>
2019-09-06 00:06:40 +0800

    dialog组件的使用

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -1,11 +1,16 @@
 <template>
-  <div></div>
+  <van-dialog v-model="show" :show-cancel-button="false" close-on-click-overlay></van-dialog>
 </template>
 
 <script>
 export default {
-  name: 'MoreAction'
-}
+  name: "MoreAction",
+  data(){
+    return {
+      show:true
+    }
+  }
+};
 </script>
 
 <style>
