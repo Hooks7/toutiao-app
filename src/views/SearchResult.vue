a30a41c54c73b06cdc04e6468e1147ed8a636f89
Hooks7 <youremail@example.com>
2019-09-08 21:13:44 +0800

    发送请求获取搜索结果的数据

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -6,20 +6,70 @@
       left-text="返回"
       left-arrow
       @click-left="$router.back()"
+      class="title"
     />
-    {{ q }}
+
+    <van-list
+    style="margin-top:46px"
+    v-model="loading"
+    :finished="finished"
+    finished-text="没有更多了"
+    @load="onLoad"
+    >
+    <van-cell
+    v-for="item in list"
+    :key ='item.art_id.toString()'
+    :title="item.title"
+    />
+
+    </van-list>
   </div>
 </div>
 </template>
 
 <script>
+import { searchResult } from '@/api/search'
 export default {
   name: 'SearchResult',
-  props: ['q']
+  props: ['q'],
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      page: 1
+    }
+  },
+  methods: {
+    async onLoad () {
+      try {
+        // 搜索结果
+        const res = await searchResult({
+          page: this.page,
+          per_page: 10,
+          q: this.q
+        })
+        // 把获取结果添加到数组
+        this.list.push(...res.results)
+        this.page++
+        this.loading = false
+
+        if (res.results.length === 0) {
+          this.loading = true
+        }
+      } catch (err) {
+        this.$toast.fail('出现了点小错误')
+      }
+    }
+  }
 
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.title{
+    position: fixed;
+    width: 100%;
+    top:0
+}
 </style>
